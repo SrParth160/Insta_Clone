f1950b32c9bf7d88ddd47a214b7cc55374a01a34
SrParth160 <parth.virani@swiftrut.com>
2024-12-28 22:22:23 +0530

    add user model

---
+++ Backend/models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    bio: {
+        type: String,
+        default: "",
+    },
+    profilePhoto: {
+        type: String,
+        default: "defaultProfilePhotoUrl",
+    },
+    followers: [{ type: ObjectId, ref: "USER" }],
+    following: [{ type: ObjectId, ref: "USER" }],
+    posts: [{ type: ObjectId, ref: "POST" }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+mongoose.model("USER", userSchema);
