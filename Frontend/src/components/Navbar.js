1b7855f48a953bd60dd9eba51bf98387f0fb43af
SrParth160 <parth.virani@swiftrut.com>
2024-12-31 22:20:51 +0530

    install dependencies

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,71 +1,10 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "./Navbar.css";
-import { IoClose, IoMenu } from "react-icons/io5";
-const Navbar = () => {
- return (
-   <header className="header">
-     <nav className="nav container">
-       <NavLink to="/" className="nav__logo">
-         Navigation Bar
-       </NavLink>
+import React from 'react'
+import logo from "../img/logo.png";
 
-       <div
-         className={"nav__menu"}
-         id="nav-menu"
-       >
-         <ul className="nav__list">
-           <li className="nav__item">
-             <NavLink to="/" className="nav__link">
-               Home
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink to="/news" className="nav__link">
-               News
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink
-               to="/about-us"
-               className="nav__link"
-             >
-               About Us
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink
-               to="/favorite"
-               className="nav__link"
-             >
-               Favorite
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink
-               to="/location"
-               className="nav__link"
-             >
-               Location
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink to="/get-started" className="nav__link nav__cta">
-               Get Started
-             </NavLink>
-           </li>
-         </ul>
-         <div className="nav__close" id="nav-close">
-           <IoClose />
-         </div>
-       </div>
-
-       <div className="nav__toggle" id="nav-toggle">
-         <IoMenu />
-       </div>
-     </nav>
-   </header>
- );
-};
-
-export default Navbar;
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <img src='logo' alt=''/>
+    </div>
+  )
+}
