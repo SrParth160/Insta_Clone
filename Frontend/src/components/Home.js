57143b8e0094a5f35c8cc170e0103e0dd79d9aff
SrParth160 <parth.virani@swiftrut.com>
2025-01-06 06:43:11 +0530

    refactor: enhance comment functionality in Home component with improved display and interaction

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -3,15 +3,14 @@ import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import { toast } from "react-toastify";
 
-
 export default function Home() {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
   const [comment, setComment] = useState("");
 
-    // Toast functions
-    const notifyERR = (message) => toast.error(message);
-    const notifySUC = (message) => toast.success(message);
+  // Toast functions
+  const notifyERR = (message) => toast.error(message);
+  const notifySUC = (message) => toast.success(message);
 
   // console.log(data);
 
@@ -129,7 +128,7 @@ export default function Home() {
             <div className="card-image">
               <img src={post.photo ? post.photo : "currept image"} />
             </div>
-            
+
             {/* {card content} */}
             <div className="card-content">
               {post.likes.includes(
@@ -164,8 +163,7 @@ export default function Home() {
             </div>
             {/* {add comment} */}
             <div className="add-comment">
-              {/* <span class="material-symbols-outlined"></span> */}
-
+              <span className="material-symbols-outlined">mood</span>
               <input
                 type="text"
                 placeholder="Add Comment"
@@ -174,16 +172,99 @@ export default function Home() {
                   setComment(e.target.value);
                 }}
               />
-              <button className="comment" 
-              onClick={() => {
-                    makeComment(comment, post._id);
-                    // toggleComment();
-              }}
-                    >post</button>
+              <button
+                className="comment"
+                onClick={() => {
+                  makeComment(comment, post._id);
+                  // toggleComment();
+                }}
+              >
+                post
+              </button>
             </div>
           </div>
         );
       })}
+      {show && (
+        <div className="showComment">
+          <div className="container">
+            <div className="postPic">
+              <img src={item.photo} alt="" />
+            </div>
+            <div className="details">
+              {/* card header */}
+              <div
+                className="card-header"
+                style={{ borderBottom: "1px solid #00000029" }}
+              >
+                <div className="card-pic">
+                  <img
+                    src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                    alt=""
+                  />
+                </div>
+                <h5>{item.postedBy.name}</h5>
+              </div>
+              {/* commentSection */}
+              <div
+                className="comment-section"
+                style={{ borderBottom: "1px solid #00000029" }}
+              >
+                {item.comments.map((comment) => {
+                  return (
+                    <p className="comm">
+                      <span
+                        className="commenter"
+                        style={{ fontWeight: "bolder" }}
+                      >
+                        {comment.postedBy.name}{" "}
+                      </span>
+                      <span className="commentText">{comment.comment}</span>
+                    </p>
+                  );
+                })}
+              </div>
+              {/* card content */}
+              <div className="card-content">
+                <p>{item.likes.length} Likes</p>
+                <p>{item.body}</p>
+              </div>
+
+              {/* add Comment */}
+              <div className="add-comment">
+                <span className="material-symbols-outlined">mood</span>
+                <input
+                  type="text"
+                  placeholder="Add a comment"
+                  value={comment}
+                  onChange={(e) => {
+                    setComment(e.target.value);
+                  }}
+                />
+                <button
+                  className="comment"
+                  onClick={() => {
+                    makeComment(comment, item._id);
+                    toggleComment();
+                  }}
+                >
+                  Post
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            className="close-comment"
+            onClick={() => {
+              toggleComment();
+            }}
+          >
+            <span className="material-symbols-outlined material-symbols-outlined-comment">
+              close
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
